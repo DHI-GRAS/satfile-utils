713712bc2f06634dcf7e2cc969c7ad6a9c15df86
Jonas Sølvsteen <j08lue@gmail.com>
2017-09-28 08:29:44 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='satfile_utils',
-    version='0.3',
+    version='0.4',
     description='Satellite Data File Utilities',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
     packages=find_packages(),
     install_requires=[
-        'satmeta>=0.4'],
+        'satmeta>=0.12'],
     dependency_links=[
-        'https://github.com/DHI-GRAS/satmeta/archive/v0.4.tar.gz#egg=satmeta-0.4'])
+        'https://github.com/DHI-GRAS/satmeta/archive/v0.12.tar.gz#egg=satmeta-0.12'])
