5ba6d73f4dbf7bb3bb0a23b64f16723f91554a92
Jonas Sølvsteen <j08lue@gmail.com>
2018-05-28 16:01:24 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='satfile_utils',
-    version='0.5.1',
+    version='0.5.2',
     description='Satellite Data File Utilities',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
